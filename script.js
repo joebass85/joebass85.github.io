38a8f943ae9c0885015348511b7cca86e167b4f1
janethediamond56 <jdiamond1721@gmail.com>
2025-07-09 20:30:00 -0400

    ffixing js file

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function onPageLoaded() {
     // Wri(te your javascript code here
     console.log("page loaded");
 
-    if (confirm("Help Us Reach Our Goal By Contributing Today!","YES!","no :(")) {
+    if (confirm("Help Us Reach Our Goal By Contributing Today!")) {
         window.open("/contribute/");
         console.log("confirmed!");
     }
