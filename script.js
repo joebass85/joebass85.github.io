e2e886aab9aa4f641fc8acd4b2255a2052c21179
janethediamond56 <jdiamond1721@gmail.com>
2025-07-13 02:13:21 -0400

    cookies???

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 let visit = 0;
 
 function onPageLoaded() {
-
     // Confirm dialog box directing to Contribute/
+    checkCookie();
     if (confirm("Help Us Reach Our Goal By Contributing Today!\n\nPlease Consider Donating!")) {
         window.open("/contribute/index.html");
+    }
+}
+
+function setCooke(cname, cvalue, exdays) {
+    const d = new Date();
+    d.settime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let username = getCookie("username");
+    if (username != "") {
+        alert("Welcome again " + username);
+    } else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
     }
 }
 
