aed35fda71f4881783aa1cdda4c1ae93e96d9c25
janethediamond56 <jdiamond1721@gmail.com>
2025-07-16 09:52:36 -0400

    maybe figuring out js? tbd

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // Confirm dialog box on Home load. Redirects to /contribute/index.html
 function onPageLoaded() {
-    if (confirm("Help Us Reach Our Goal By Contributing Today!\n\nPlease Consider Donating!")) {
-        window.open("/contribute/index.html");
+    sessionStorage.setItem("isVisited", 0);
+    let val = sessionStorage.getItem("isVisited");
+    if (val == 0) {
+        if (confirm("Help Us Reach Our Goal By Contributing Today!\n\nPlease Consider Donating!")) {
+            window.open("/contribute/index.html");
+            sessionStorage.setItem("isVisited",1);
+            let val2 = sessionStorage.getItem("isVisited");
+            console.log("isVisited:" + val2);
+        }
+    } else {
+        let val1 = sessionStorage.getItem("isVisited");
+        console.log("isVisited:" + val1);
     }
 }
 
